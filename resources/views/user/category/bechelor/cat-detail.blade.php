be4bbd1bd36f2dfc2d19572039adb397b23c8325
ismail hosen <ismailwdpf@gmail.com>
2023-11-07 00:21:04 +0600

    detail

--- resources/views/user/category/bechelor/cat-detail.blade.php
+++ resources/views/user/category/bechelor/cat-detail.blade.php
@@ -11,15 +11,18 @@
                         <h1>BA (Hons) Business Management (Marketing)</h1>
 
                         <div class="my-5">
-                            
+
                             <div>
                                 <p> Share it:</p>
                                 <a href="#"><i class="shareicon fa-brands fa-facebook" style="color: #075ef2;"></i></a>
-                                <a href="#"><i class="shareicon fa-brands fa-facebook-messenger" style="color: #0459ec;"></i></a>
-                                <a href="#"><i class="shareicon fa-brands fa-linkedin" style="color: #055bf0;"></i></a>
+                                <a href="#"><i class="shareicon fa-brands fa-facebook-messenger"
+                                        style="color: #0459ec;"></i></a>
+                                <a href="#"><i class="shareicon fa-brands fa-linkedin"
+                                        style="color: #055bf0;"></i></a>
                                 <a href="#"><i class="shareicon fa-solid fa-envelope" style="color: #86898d;"></i></a>
-                                <a href="#"><i class="shareicon fa-sharp fa-solid fa-share-nodes" style="color: #2cd84e;"></i></a>
-                               
+                                <a href="#"><i class="shareicon fa-sharp fa-solid fa-share-nodes"
+                                        style="color: #2cd84e;"></i></a>
+
 
                             </div>
 
@@ -41,17 +44,35 @@
                         <span>Institute:</span>
                         <h5>DEREE - The American College of Greece</h5>
                         <a href="#">See all Bachelors offered by this institute</a>
-                    </div>                  
+                    </div>
                 </div>
 
                 <div class="p-4 mx-5">
-                 <a href=""><i class="fa-solid fa-square-phone"></i></a>   +88 210 600 9800
+                    <a href=""><i class="fa-solid fa-square-phone"></i></a> +88 210 600 9800
                 </div>
             </div>
 
+            <div class="icon-card my-2">
+                <span><i class="fa-solid fa-location-dot"></i> Athens, Thessaloniki</span>
+                <span> <i class="fa-regular fa-calendar"></i> February 2024, October 2024</span>
+                <span> <i class="fa-regular fa-hourglass-half"></i> 3 years</span>
+                <span> <i class="fa-regular fa-id-card"></i> Bachelor</span>
+                <span> <i class="fa-regular fa-house"></i> Full-time</span>
+                <span><i class="fa-solid fa-earth-americas"></i> Greek, English</span>
+            </div>
+        </div>
+<div>
+
+    <div class="container row">
+        <div class="col-9">
+Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo, provident quasi. Iste sint nemo beatae qui obcaecati ipsam error! Excepturi, facilis. Repudiandae est voluptatibus voluptates velit eligendi inventore rerum assumenda.
         </div>
 
-
+        <div class="col-3">
+Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea sit facilis quos eveniet ex similique error amet molestiae? Voluptates pariatur aperiam, voluptatem nulla molestiae odit fugit illo minus distinctio ea.
+        </div>
+    </div>
+</div>
 
     </div>
     <br>
